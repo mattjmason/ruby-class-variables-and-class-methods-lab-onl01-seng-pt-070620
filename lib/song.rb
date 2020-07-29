d44b5ba75348f8c226439e14e0629cf5e41057a4
Matt Mason <masonmj10@gmail.com>
2020-07-29 14:23:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,7 +8,10 @@ class Song
   @@artist_count = {}
   
   
-  def initialize 
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist 
+    @genre = genre 
     @@count += 1 
   end
   
